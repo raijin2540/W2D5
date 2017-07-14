851fc0cd7e18412a4aebab3d744d1e9c95d6577a
Ellie Wawrzaszek <elliewawrzaszek@gmail.com>
2017-07-14 16:47:29 -0400

    Finished phase two

--- my_hash_map/lib/p02_hashing.rb
+++ my_hash_map/lib/p02_hashing.rb
@@ -4,12 +4,24 @@ end
 
 class Array
   def hash
-    self.hash
+    str = ""
+    self.each_with_index do |el, idx|
+      str += (el.to_s + idx.to_s)
+    end
+
+    str.to_i.hash.to_i
   end
 end
 
 class String
   def hash
+    arr = self.split("").map { |el| el.ord}
+    str = ""
+    arr.each_with_index do |el, idx|
+      str += (el.to_s + idx.to_s)
+    end
+
+    str.to_i.hash.to_i
   end
 end
 
@@ -17,6 +29,18 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    arr = []
+    self.to_a.sort.each_with_index do |tuple, idx|
+      tuple << idx
+      arr << tuple.map {|el| el.to_s}
+    end
+    arr.flatten.join.hash.to_i
+    # arr.join.str = ""
+    # self.each_with_index do |el, idx|
+    #   str += (el.to_s + idx.to_s)
+    # end
+
+    # str_arr.to_i.hash.to_i
+
   end
 end
